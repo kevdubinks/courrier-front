77e381fc63bc8f43422e23c85a46f06f09dca5a9
ndoyeKevin <kevin.ndoye@institutsolacroup.com>
2024-01-18 18:00:49 +0100

    up

---
+++ ProtectedRoute.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+const ProtectedRoute = ({ children, isAdminRoute = false }) => {
+  const token = localStorage.getItem('token');
+  const isAdmin = localStorage.getItem('isAdmin') === 'true';
+
+  if (!token || (isAdminRoute && !isAdmin)) {
+    return <Navigate to="/" />;
+  }
+
+  return children;
+};
