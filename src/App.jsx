b2df33aaf81a02e1d50c960007e1dc02653e7d3c
ndoyeKevin <kevin.ndoye@institutsolacroup.com>
2024-01-18 17:54:33 +0100

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/login.jsx"; // Importez votre composant Login
-// import DashboardClient from "./DashboardClient"; // Remplacez par votre composant de dashboard client
-// import DashboardGestionnaire from "./DashboardGestionnaire"; // Remplacez par votre composant de dashboard gestionnaire
+ import DashboardClient from "./DashboardClient"; // Remplacez par votre composant de dashboard client
+ import DashboardAdmin from "./DashboardAdmin"; // Remplacez par votre composant de dashboard gestionnaire
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        {/* <Route path="/client-dashboard" element={<DashboardClient />} />
-        <Route path="/manager-dashboard" element={<DashboardGestionnaire />} /> */}
+        <Route path="/client-dashboard" element={<DashboardClient />} />
+        <Route path="/Admin-dashboard" element={<DashboardAdmin />} />
         {/* Ajoutez d'autres routes selon vos besoins */}
       </Routes>
     </Router>
